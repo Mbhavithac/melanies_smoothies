b8df7228f760970ab4079b2b70481004ce8a7e9d
Mbhavithac <133844630+Mbhavithac@users.noreply.github.com>
2026-08-28 22:42:14 +0530

    Refactor fruit data retrieval and API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,14 @@ st.write(
     "The name on your smoothie will be",
     name_on_order
 )
-fruit_df = (
-    session.table("smoothies.public.fruit_options")
-    .select(col("FRUIT_NAME"))
-    .collect()
-)
-fruit_list = [row["FRUIT_NAME"] for row in fruit_df]
+my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'),col('SEARCH_ON'))
+fruit_list = [row["FRUIT_NAME"] for row in my_dataframe]
+# st.dataframe(data=my_dataframe,use_container_width=True)
+# st.stop()
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     fruit_list,
@@ -29,8 +31,9 @@ if ingredients_list:
     ingredients_string = ""
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + " "
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits_chosen, 'SEARCH_ON'].iloc[0]
         st.subheader(fruits_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     time_to_insert = st.button("Submit Order")
