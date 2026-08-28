0b08347188bd85f57c971d5efd378ec462b337d1
Mbhavithac <133844630+Mbhavithac@users.noreply.github.com>
2026-08-28 23:18:14 +0530

    Refactor smoothie name input and success message

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ st.write(
 )
 
 
-name_on_order = st.text_input("Name of Smoothie")
-st.write("The name of your smoothie will be: ", name_on_order)
+name_on_order = st.text_input("Name on Smoothie")
+st.write("The name on your smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -34,14 +34,14 @@ if ingredients_list:
     
     ingredients_string =''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string +=fruit_chosen +' '
+    for fruits_chosen in ingredients_list:
+        ingredients_string +=fruits_chosen +' '
         
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits_chosen, 'SEARCH_ON'].iloc[0]
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
       
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruits_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/"+search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
@@ -57,4 +57,7 @@ if ingredients_list:
     
         if ingredients_string:
             session.sql(my_insert_stmt).collect()
-            st.success('Your Smoothie '+name_on_order+' is ordered!', icon="✅")
+          st.success(
+             f"Your Smoothie is ordered, {name_on_order}!",
+            icon="✅"
+          )
